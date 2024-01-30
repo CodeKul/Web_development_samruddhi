ec2f99110f85c4dcfcbab546e082358c92f9126a
samnichal321 <sn202325@appystack.com>
2024-01-30 15:17:43 +0530

    git operators

---
+++ Web_Devlopment/JavaScript/Operators/operators.js
@@ -0,0 +1,56 @@
+// Arithmetic Operators
+
+// Addition
+// Substraction
+// Division
+// Multiplication
+// Remainder/Modulus
+// Increment/Decrement
+
+//Assignment Operators
+
+// Assignment 
+// Compound Assignment
+
+// Comparison Operators
+
+// Equal to (==),Strict Equal to (===)
+5 == "5";   // true (loose equality)
+5 === "5";  // false (strict equality)
+
+// Not Equal to(!=), Strict Not Equal to (!==)
+5 != "5";   // false (loose inequality)
+5 !== "5";  // true (strict inequality)
+
+// Greater than (>) and Less than (<):
+let a = 10;
+let b = 5;
+a > b;  // true
+a < b;  // false
+
+
+// Logical Operators   :
+
+// Logical AND (&&), OR (||), and NOT (!):
+
+let condition1 = true;
+let condition2 = false;
+
+condition1 && condition2;  // false (AND)
+condition1 || condition2;  // true (OR)
+!condition1;    // false
+
+// Other Operators 
+
+// Ternary Operator (Conditional Operator):
+ let result = (condition) ? "true" : "false";
+
+
+//Typeof Operator:
+
+typeof "Hello";  // "string"
+typeof 42;       // "number"
+
+//Instanceof Operator:
+
+// obj instanceof Array; 

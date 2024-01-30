fcdde9b1f50cb71fa466cfe6197714d1596326f1
samnichal321 <sn202325@appystack.com>
2024-01-30 14:53:50 +0530

    jsdatype

---
+++ Web_Devlopment/JavaScript/DataTypes/datatype.js
@@ -0,0 +1,59 @@
+// Primitive Data type
+// 1.Undefined :
+// Represents an uninitialized or undeclared variable.
+// let undefinedVar;
+
+// 2.Null :
+// Represents the intentional absence of any object value.
+// let nullVar = null;
+
+// 3.Boolean :
+// Represents a logical entity and can have two values: true or `false.
+// let isTrue = true;
+// let isFalse = false;
+
+//  4.Number :
+//  Represents both integers and floating-point numbers.
+//  let integerNumber = 42;
+//  let floatingPointNumber = 3.14;
+
+// 5.Bigint :
+// Represents arbitrary precision integers
+// let bigIntNumber = 1234567890123456789012345678901234567890n;
+
+// 6.String :
+// Represents sequences of characters.
+// let greeting = "Hello, World!";
+
+// 7.Symbol:
+// Represents a unique identifier.
+// let sym = Symbol("uniqueSymbol");
+
+// Non-primitive Datatypes
+
+// 1.Object:
+// Represents a collection of key-value pairs (properties and methods).
+// let person = {
+//     name: "John",
+//     age: 30,
+//     city: "Exampleville"
+//   };
+
+// 2.Array
+// Represents ordered lists of values.
+// let numbers = [1, 2, 3, 4, 5];
+
+// 3.function:
+// Represents a reusable block of code.
+// function greet(name) {
+//     console.log("Hello, " + name + "!");
+//   }
+
+// 4.Date :
+// Represents dates and times.'
+// let currentDate = new Date();
+
+// 5.RegExp:
+//  Represents regular expressions.
+//  let regex = /[a-z]+/;
+

554c0eb06f600ddd86b6cb8d808752090ae0f1e6
samnichal321 <sn202325@appystack.com>
2024-01-30 15:16:20 +0530

    datatype

---
+++ Web_Devlopment/JavaScript/DataTypes/script.js
@@ -0,0 +1,46 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+let num = 13;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+const employee = [
+    {
+    salary : "870000",
+    job : "software Engineer"
+},
+{
+    salary : "870000",
+    job : "software Engineer"
+},
+
+]
+
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+let string = 16 + "strings"
+console.log(string);
+//string
+//16strings
+
+let a = "abcd" + 10 + 30
+console.log(a)
+
+let str = "5" + 7 + 10
+console.log(str)

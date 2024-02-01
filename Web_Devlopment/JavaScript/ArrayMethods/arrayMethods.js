e2fff2b831f39df43ba1f146bfee7cf45daebdc8
samnichal321 <sn202325@appystack.com>
2024-02-01 15:14:27 +0530

    array

---
+++ Web_Devlopment/JavaScript/ArrayMethods/arrayMethods.js
@@ -0,0 +1,21 @@
+// 1.push(item1,item2): Adds one or more elements to the end of an array and returns the new length.
+// 2.pop() :  Removes the last element from an array and returns that element.
+// 3.shift(): Removes the first element from an array and returns that element.
+// 4.unshift(): Adds one or more elements to the beginning of an array and returns the new length.
+// 5.concat(): Returns a new array by combining multiple arrays or values.
+// 6.slice(): Returns a shallow copy of a portion of an array.
+// 7.splice():Changes the contents of an array by removing or replacing existing elements and/or
+// adding new elements.
+// 8.indexOf(item, fromIndex): Returns the first index at which a given element 
+//can be found in the array, or -1 if it is not present.
+// 9.lastIndexOf(item, fromIndex): Returns the last index at which a given 
+//element can be found in the array, or -1 if it is not present.
+// 10.forEach(callback(item, index, array)): Calls a provided function once 
+//for each element in the array, in order.
+// 11.map(callback(item, index, array)):Creates a new array with the results of 
+//calling a provided function on every element in the array.
+// 12.filter(callback(item, index, array)):Creates a new array with all elements 
+//that pass the test implemented by the provided function
+// 13.reduce(callback(accumulator, currentValue, index, array), initialValue): 
+//Applies a function against an accumulator and each element in the array (from left to right) to 
+//reduce it to a single value.

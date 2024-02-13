3adb2499120bce4c06c40f9d05cdca5ef921c549
samnichal321 <sn202325@appystack.com>
2024-02-13 15:12:34 +0530

    truthyValue

---
+++ Web_Devlopment/JavaScript/truthyFalsyValue/truthy.js
@@ -0,0 +1,24 @@
+var result = 0;
+console.log(result)//0
+
+var a = 10 == 10;
+console.log(a)//false
+
+var a = 1; //
+
+var area = -1;
+console.log(area)//-1
+
+
+var string = "j";
+console.log(string)//j
+ 
+var str = [``]
+console.log(str)//undefined,null
+
+var  a = "Hello World"
+console.log(a)
+var  a = 'Hello World'
+console.log(a)
+var  a = `Hello World`
+console.log(a)

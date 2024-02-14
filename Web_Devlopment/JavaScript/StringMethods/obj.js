efe9b3d1bdf3c192323f410175826642f6333a18
samnichal321 <sn202325@appystack.com>
2024-02-14 14:54:03 +0530

    object

---
+++ Web_Devlopment/JavaScript/StringMethods/obj.js
@@ -0,0 +1,335 @@
+const scheduleData = [
+    {
+      date: "20 January 2024",
+      count: 5,
+      surgeryDetails: [
+        {
+          startTime: "9 AM",
+          endTime: "11 AM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+            {
+              surgeonsTheater: "Surgery 2 (Theater 1)",
+              surgeons: [{ name: "Mahesh Salunkhe , Rajesh Jarange" }],
+            },
+          ],
+        },
+        {
+          startTime: "11 AM",
+          endTime: "12 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "12 PM",
+          endTime: "1 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "2 PM",
+          endTime: "3 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "4 PM",
+          endTime: "6 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "6 PM",
+          endTime: "9 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      date: "21 January 2024",
+      count: 4,
+      surgeryDetails: [
+        {
+          startTime: "9 AM",
+          endTime: "11 AM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "11 AM",
+          endTime: "12 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Theater1",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "12 PM",
+          endTime: "1 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Theater1",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      date: "20 January 2024",
+      count: 5,
+      surgeryDetails: [
+        {
+          startTime: "9 AM",
+          endTime: "11 AM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+            {
+              surgeonsTheater: "Surgery 2 (Theater 1)",
+              surgeons: [{ name: "Mahesh Salunkhe , Rajesh Jarange" }],
+            },
+          ],
+        },
+        {
+          startTime: "11 AM",
+          endTime: "12 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "12 PM",
+          endTime: "1 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "2 PM",
+          endTime: "3 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "4 PM",
+          endTime: "6 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "6 PM",
+          endTime: "9 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      date: "21 January 2024",
+      count: 4,
+      surgeryDetails: [
+        {
+          startTime: "9 AM",
+          endTime: "11 AM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "11 AM",
+          endTime: "12 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Theater1",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "12 PM",
+          endTime: "1 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Theater1",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      date: "20 January 2024",
+      count: 5,
+      surgeryDetails: [
+        {
+          startTime: "9 AM",
+          endTime: "11 AM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+            {
+              surgeonsTheater: "Surgery 2 (Theater 1)",
+              surgeons: [{ name: "Mahesh Salunkhe , Rajesh Jarange" }],
+            },
+          ],
+        },
+        {
+          startTime: "11 AM",
+          endTime: "12 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "12 PM",
+          endTime: "1 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "2 PM",
+          endTime: "3 PM",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "4 PM",
+          endTime: "6 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "6 PM",
+          endTime: "9 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      date: "21 January 2024",
+      count: 4,
+      surgeryDetails: [
+        {
+          startTime: "9 AM",
+          endTime: "11 AM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Surgery 1 (Theater 1)",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "11 AM",
+          endTime: "12 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Theater1",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+        {
+          startTime: "12 PM",
+          endTime: "1 PM",
+          theater: "Theater 1",
+          theaters: [
+            {
+              surgeonsTheater: "Theater1",
+              surgeons: [{ name: "Amit Raande , Yogesh Patil" }],
+            },
+          ],
+        },
+      ],
+    },
+  ];

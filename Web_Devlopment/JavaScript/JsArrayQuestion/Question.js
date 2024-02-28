2245374f1cdefc121c55e328c37fd39b0688aaee
samnichal321 <sn202325@appystack.com>
2024-02-28 15:18:30 +0530

    question

---
+++ Web_Devlopment/JavaScript/JsArrayQuestion/Question.js
@@ -0,0 +1,12 @@
+// Write a function filterEvenNumbers that takes an array of numbers as input and returns a 
+// new array containing only the even numbers.
+
+
+
+//Given an array of strings, write a function findLongestString that 
+//returns the longest string from the array.
+
+
+
+// Write a function sumPositiveNumbers that takes an array of numbers as input and returns
+//  the sum of all positive numbers in the array.
